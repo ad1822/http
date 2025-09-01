8382532efe2c0183c479cc4b7cb77984ce3b65b3
Ayush <ayushdumasia00@gmail.com>
2025-09-01 16:50:01 +0530

    feat(chapter 1): Read 8 bytes from file and print. Using channel, buffer

---
+++ httpfromtcp/go.mod
@@ -0,0 +1,3 @@
+module github.com/ad1822/httpfromtcp
+
+go 1.24.6
---
+++ httpfromtcp/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+			if err == io.EOF {
+				break
+			}
+
+			buffer = buffer[:n]
+			if i := bytes.IndexByte(buffer, '\n'); i != -1 {
+				str += string(buffer[:i])
+				buffer = buffer[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(buffer)
+		}
+	}()
+	return out
+}
+
+func main() {
+	openFile, err := os.Open("message.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer openFile.Close()
+
+	lines := getLinesChannel(openFile)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+}

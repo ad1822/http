d6caf7fbb1942c12526b5a8655f35173f0518d0c
Ayush <ayushdumasia00@gmail.com>
2025-09-05 16:53:21 +0530

    fix(headers): Handle multiple values for single key

--- httpfromtcp/internal/headers/headers_test.go
+++ httpfromtcp/internal/headers/headers_test.go
@@ -28,4 +28,11 @@ func TestHeaders(t *testing.T) {
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
 
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nHost: localhost:42060\r\n")
+	n, done, err = headers.Parse(data)
+	// require.NoError(t, err)
+	// require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069,localhost:42060", headers.Get("HOST"))
+	// assert.True(t, done)
 }
